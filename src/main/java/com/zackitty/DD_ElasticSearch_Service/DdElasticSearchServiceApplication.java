bff49efa613baa1934cc1e6db9e9d70ab8a29bfd
zackitty <theelfenliedpictureshow@yahoo.com>
2022-11-02 13:31:52 -0500

    init

---
+++ src/main/java/com/zackitty/DD_ElasticSearch_Service/DdElasticSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zackitty.DD_ElasticSearch_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdElasticSearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdElasticSearchServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zackitty/DD_ElasticSearch_Service/DdElasticSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zackitty.DD_ElasticSearch_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdElasticSearchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
